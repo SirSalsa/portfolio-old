c42078aa6bc8a955b0e2b5c94d6ee086bbf0ab44
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-08-19 11:55:10 +0200

    Added more skills and icons

--- src/modules/Skills.jsx
+++ src/modules/Skills.jsx
@@ -6,6 +6,9 @@ export default function Skills() {
     const css_url = new URL('../media/skills/css.png', import.meta.url);
     const js_url = new URL('../media/skills/js.png', import.meta.url);
     const react_url = new URL('../media/skills/reactjs.png', import.meta.url);
+    const mysql_url = new URL('../media/skills/mysql.png', import.meta.url);
+    const restful_url = new URL('../media/skills/api_rest.png', import.meta.url);
+    const github_url = new URL('../media/skills/github.png', import.meta.url);
 
     return (
         <>
@@ -16,24 +19,36 @@ export default function Skills() {
             </div>
             <div className="skills_wrapper">
                 <article>
-                    <img id="skills_img" src={java_url} alt="HTML Logo" />
+                    <img id="skills_img" src={java_url} alt="Java Logo" />
                     <p id="skills_text">Java</p>
+                </article>
+                <article>
+                    <img id="skills_img" src={js_url} alt="Javascript Logo" />
+                    <p id="skills_text">JavaScript</p>
                 </article>
                 <article>
                     <img id="skills_img" src={html_url} alt="HTML Logo" />
                     <p id="skills_text">HTML</p>
                 </article>
                 <article>
-                    <img id="skills_img" src={css_url} alt="HTML Logo" />
+                    <img id="skills_img" src={css_url} alt="CSS Logo" />
                     <p id="skills_text">CSS</p>
                 </article>
                 <article>
-                    <img id="skills_img" src={js_url} alt="HTML Logo" />
-                    <p id="skills_text">JavaScript</p>
+                    <img id="skills_img" src={react_url} alt="React.JS Logo" />
+                    <p id="skills_text">React JS</p>
                 </article>
                 <article>
-                    <img id="skills_img" src={react_url} alt="HTML Logo" />
-                    <p id="skills_text">React</p>
+                    <img id="skills_img" src={mysql_url} alt="MySQL Logo" />
+                    <p id="skills_text">MySQL</p>
+                </article>
+                <article>
+                    <img id="skills_img" src={restful_url} alt="Restful API Logo" />
+                    <p id="skills_text">RESTful API</p>
+                </article>
+                <article>
+                    <img id="skills_img" src={github_url} alt="Github Logo" />
+                    <p id="skills_text">Github</p>
                 </article>
             </div>
         </>
