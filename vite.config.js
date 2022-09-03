b26d85d3b7dcdc6e9c2191a17d9c6b7affaed911
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-09-03 22:10:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://torstenlareke.com/",
+  base: "https://torstenlareke.com/",
   plugins: [react()]
 })
